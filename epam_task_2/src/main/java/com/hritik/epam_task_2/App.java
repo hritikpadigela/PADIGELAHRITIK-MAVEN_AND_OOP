a444d100c9cac47df5f0e1230663cfd17a989ac6
hritikpadigela <hritikpadigela98@gmail.com>
2020-02-08 23:49:05 +0530

    Commit1

---
+++ epam_task_2/src/main/java/com/hritik/epam_task_2/App.java
@@ -0,0 +1,10 @@
+package com.hritik.epam_task_2;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Application app= new Application();
+        app.start();
+    }
+}
